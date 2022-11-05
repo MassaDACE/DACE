4b44748aab271e4b0556c7578ad21248631c0b49
AurelienFT <aurelien.foucault@epitech.eu>
2022-11-06 00:42:56 +0100

    Edit attribute

--- dace-front/src/components/creation/containers/AttributeContainer.tsx
+++ dace-front/src/components/creation/containers/AttributeContainer.tsx
@@ -9,9 +9,16 @@ enum Type {
     Text = "Text",
 }
 
+enum StateModal {
+    Create = "Create",
+    Edit = "Edit"
+}
+
 function AttributeContainer() {
-    const [open, setOpen] = React.useState(false)
-    const createButtonRef = React.useRef(null)
+    const [open, setOpen] = React.useState(false);
+    const [idModal, setIdModal] = React.useState<number | null>(null);
+    const [stateModal, setStateModal] = React.useState(StateModal.Create);
+    const createButtonRef = React.useRef(null);
     const [attribute, setAttribute] = React.useState({ name: "", type: Type.Text, value: "" });
     const [attributes, setAttributes] = React.useState([{ name: "Picture", type: Type.Image, value: "testlolololefouvhriuhrviuhrviurvhiurvhiruhriuvhr" }]);
 
@@ -29,8 +36,23 @@ function AttributeContainer() {
 
     const addAttribute = () => {
         setAttributes([...attributes, attribute]);
+        setAttribute({ name: "", type: Type.Text, value: "" });
         setOpen(false);
     }
+
+    const editAttribute = (index: number) => {
+        setAttributes(attributes.map((attributeElem, i) => {
+            if (i === index) {
+                attributeElem.name = attribute.name;
+                attributeElem.type = attribute.type;
+                attributeElem.value = attribute.value;
+            }
+            return attributeElem;
+        }));
+        setAttribute({ name: "", type: Type.Text, value: "" });
+        setOpen(false);
+    }
+
 
     const removeAttribute = (index: number) => {
         setAttributes(attributes.filter((_, i) => i !== index));
@@ -40,7 +62,7 @@ function AttributeContainer() {
         <div className="flex border-r-2 border-b-2 h-full overflow-auto">
 
             <Transition.Root show={open} as={React.Fragment}>
-                <Dialog as="div" className="relative z-10" initialFocus={createButtonRef} onClose={setOpen}>
+                <Dialog as="div" className="relative z-10" initialFocus={createButtonRef} onClose={() => {setAttribute({ name: "", type: Type.Text, value: "" }); setOpen(false);}}>
                     <Transition.Child
                         as={React.Fragment}
                         enter="ease-out duration-300"
@@ -73,7 +95,7 @@ function AttributeContainer() {
                                             Name:
                                         </div>
                                         <div>
-                                            <input className="border-2 rounded" onChange={(e) => changeTempName(e)} type="text" />
+                                            <input className="border-2 rounded" value={attribute.name} onChange={(e) => changeTempName(e)} type="text" />
                                         </div>
                                     </div>
                                     <div className="flex py-4">
@@ -100,18 +122,25 @@ function AttributeContainer() {
                                             Default value:
                                         </div>
                                         <div>
-                                            <input className="border-2 rounded" onChange={(e) => changeTempValue(e)} type="text" />
+                                            <input className="border-2 rounded" value={attribute.value} onChange={(e) => changeTempValue(e)} type="text" />
                                         </div>
                                     </div>
                                     <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                                        <button
+                                        { stateModal == StateModal.Create ? <button
                                             type="button"
                                             className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                                             onClick={() => addAttribute()}
                                             ref={createButtonRef}
                                         >
                                             Create Attribute
-                                        </button>
+                                        </button> : <button
+                                            type="button"
+                                            className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                                            onClick={() => editAttribute(idModal!)}
+                                            ref={createButtonRef}
+                                        >
+                                            Edit Attribute
+                                        </button> }
                                     </div>
                                 </Dialog.Panel>
                             </Transition.Child>
@@ -140,13 +169,13 @@ function AttributeContainer() {
                             {attribute.value.length > 20 ? attribute.value.substring(0, 20) + "..." : attribute.value}
                         </div>
                         <div className="flex">
-                            <PencilSquareIcon className="w-4" />
+                            <PencilSquareIcon className="w-4" onClick={() => { setAttribute({name: attribute.name, value: attribute.value, type: attribute.type}); setStateModal(StateModal.Edit); setIdModal(index); setOpen(true); }} />
                             <XMarkIcon className="w-4" onClick={() => removeAttribute(index)} />
                         </div>
                     </div>
                 ))}
                 <div className="flex justify-center">
-                    <button className="bg-red-400 border-2 rounded-full px-4 py-2" onClick={() => {setOpen(true)}}> Add new attribute</button>
+                    <button className="bg-red-400 border-2 rounded-full px-4 py-2" onClick={() => {setStateModal(StateModal.Create); setOpen(true);}}> Add new attribute</button>
                 </div>
             </div>
         </div>
